ec5ea1a265009ef5cafb26650c02344ea23191bb
Aman Anurag <aman.anurag@ibm.com>
2021-07-05 09:19:57 +0530

    largest element in array

---
+++ largest-in-array.js
@@ -0,0 +1,13 @@
+/*
+Find the largest element in array
+e.g arr[2,6,44,34] Ans = 44
+*/
+function largestFinder(arr){
+    var max = arr[0];
+    for(let i=0; i < arr.length; i++){
+        max = max > arr[i]?max:arr[i];
+    }
+     return max;
+}
+console.log(largestFinder([2,6,44,35]));
+
