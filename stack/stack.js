1557a38198ce82b8b94276110cb3afb4d498a941
Aman Anurag <aman.anurag@ibm.com>
2021-07-05 12:05:32 +0530

    stack ds added with push and pop

---
+++ stack/stack.js
@@ -0,0 +1,47 @@
+class Node {
+    constructor(value){
+        this.data = value;
+        this.next = null;
+
+    }
+}
+class Stack {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    push(value){
+        var node = new Node(value);
+        if(!this.first){
+            this.first = node;
+            this.last = node;
+           
+        }else{
+            var current = this.first;
+            this.first = node;
+            this.first.node = current;
+            
+        }
+        this.size++;
+        return this;
+    }
+    pop(){
+        if(!this.first) return undefined;
+        var current = this.first;
+        this.first = current.next;
+        this.size--;
+        if(size ===0){
+            this.first = null;
+            this.last = null;
+        }
+        return current;
+    }
+}
+var stack = new Stack();
+stack.push('here is function in global');
+stack.push('function added on global');
+stack.pop();
+stack.pop();
+stack.push('hello world');
+console.log(stack);
