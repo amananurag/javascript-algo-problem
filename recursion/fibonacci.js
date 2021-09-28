e1f0b398c4191d7b87dee8843ad66ad6fdc15f30
aman anurag <aman.anurag07>
2021-09-28 11:05:19 +0530

    fibonacci suing recursion

---
+++ recursion/fibonacci.js
@@ -0,0 +1,18 @@
+// calculate fibonacci for nth number 
+// 0,1,1,2,3,5,8 
+// fibonacci seq is equal to sum of last two number
+// f(0) = 0
+// f(1) = 1
+// f(2) = f(1) + f(0)
+function fib(n) {
+    if (n < 2) return n;
+    else {
+      return fib(n-1) + fib(n-2)
+    }
+  }
+function printFib(n) {
+    for(var i=0; i < n; i++){
+        console.log(fib(i))
+    }
+}
+console.log(printFib(5));
