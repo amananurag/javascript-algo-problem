b44e4d593b6ab1984c42064d818861caa5ce0c31
aman anurag <aman.anurag07>
2022-02-22 17:33:57 +0530

    added a palindrome

---
+++ strings/palindrome.js
@@ -0,0 +1,11 @@
+function palindrome(str){
+    var len = str.length/2;
+    for(let i=0; i < len; i++){
+        if(str[i]!== str[str.length-1-i]){
+            return "Not a palindrome";
+        }else{
+            return "is a palindrome";
+        }
+    }
+}
+console.log(palindrome('level'));
